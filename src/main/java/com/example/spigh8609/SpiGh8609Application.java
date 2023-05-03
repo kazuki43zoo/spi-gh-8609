8adbbfb87bf73a44e15445d118f0c0856238b20a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2023-05-03 10:53:02 +0900

    first commit

---
+++ src/main/java/com/example/spigh8609/SpiGh8609Application.java
@@ -0,0 +1,131 @@
+package com.example.spigh8609;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ApplicationObjectSupport;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Transformers;
+import org.springframework.integration.ip.dsl.Tcp;
+import org.springframework.integration.ip.tcp.connection.AbstractServerConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnection;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorFactoryChain;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorSupport;
+import org.springframework.integration.ip.tcp.serializer.TcpCodecs;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@SpringBootApplication
+public class SpiGh8609Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpiGh8609Application.class, args);
+  }
+
+  @Bean
+  public TcpConnectionInterceptorFactoryChain tcpConnectionInterceptorFactoryChain(
+      List<TcpConnectionInterceptorFactory> tcpConnectionInterceptorFactories) {
+    TcpConnectionInterceptorFactoryChain chain = new TcpConnectionInterceptorFactoryChain();
+    chain.setInterceptor(tcpConnectionInterceptorFactories.toArray(new TcpConnectionInterceptorFactory[0]));
+    return chain;
+  }
+
+  @Bean
+  public AbstractServerConnectionFactory serverConnectionFactory(
+      TcpConnectionInterceptorFactoryChain chain) {
+    return Tcp.netServer(1234)
+        .deserializer(TcpCodecs.crlf())
+        .interceptorFactoryChain(chain)
+        .get();
+  }
+
+  @Bean
+  public IntegrationFlow tcpServerInbound(AbstractServerConnectionFactory serverConnectionFactory,
+      BlockingQueue<String> tcpServerReceivedMessageQueue) {
+    return IntegrationFlows.from(Tcp.inboundAdapter(serverConnectionFactory))
+        .transform(Transformers.objectToString())
+        .log()
+        .handle(m -> tcpServerReceivedMessageQueue.offer((String) m.getPayload()))
+        .get();
+  }
+
+  @Bean
+  public IntegrationFlow tcpServerOutbound(AbstractServerConnectionFactory serverConnectionFactory) {
+    return IntegrationFlows.from("tcpServerOutboundChannel")
+        .log()
+        .handle(Tcp.outboundAdapter(serverConnectionFactory))
+        .get();
+  }
+
+  @Bean
+  public BlockingQueue<String> tcpServerReceivedMessageQueue() {
+    return new LinkedBlockingQueue<>();
+  }
+
+  public static class CharacterAppendingInterceptor extends TcpConnectionInterceptorSupport {
+    private final String value;
+
+    private CharacterAppendingInterceptor(String value, ApplicationEventPublisher publisher) {
+      super(publisher);
+      this.value = value;
+    }
+
+    @Override public void send(Message<?> message) {
+      // append specified character and delegate sending message using super class method
+      byte[] payload = (byte[]) message.getPayload();
+      super.send(MessageBuilder.withPayload((new String(payload) + value).getBytes()).copyHeaders(message.getHeaders())
+          .build());
+    }
+
+    @Override public boolean onMessage(Message<?> message) {
+      // append specified character and delegate handling message using super class method
+      byte[] payload = (byte[]) message.getPayload();
+      return super.onMessage(
+          MessageBuilder.withPayload((new String(payload) + value).getBytes()).copyHeaders(message.getHeaders())
+              .build());
+    }
+
+    static class Factory extends ApplicationObjectSupport implements TcpConnectionInterceptorFactory {
+      private final String value;
+
+      public Factory(String value) {
+        this.value = value;
+      }
+
+      @Override public TcpConnectionInterceptorSupport getInterceptor() {
+        return new CharacterAppendingInterceptor(value, obtainApplicationContext());
+      }
+    }
+  }
+
+// The batch interceptor for binding a connection that wrapped all interceptor to TcpSender(in this case TcpSendingMessageHandler)
+public static class PatchInterceptor extends TcpConnectionInterceptorSupport {
+
+  private PatchInterceptor(ApplicationEventPublisher publisher) {
+    super(publisher);
+  }
+
+  @Override public void addNewConnection(TcpConnection connection) {
+    super.addNewConnection(connection);
+    Optional.ofNullable(getSender()).ifPresent(x -> x.addNewConnection(getTheConnection()));
+  }
+
+  static class Factory extends ApplicationObjectSupport implements TcpConnectionInterceptorFactory {
+
+    @Override public TcpConnectionInterceptorSupport getInterceptor() {
+      return new PatchInterceptor(obtainApplicationContext());
+    }
+
+  }
+
+}
+
+}
---
+++ src/test/java/com/example/spigh8609/SpiGh8609ApplicationTests.java
@@ -0,0 +1,90 @@
+package com.example.spigh8609;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.integration.ip.IpHeaders;
+import org.springframework.integration.ip.tcp.connection.AbstractServerConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorFactory;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest(classes = { SpiGh8609Application.class, SpiGh8609ApplicationTests.LocalContext.class })
+@DirtiesContext
+class SpiGh8609ApplicationTests {
+
+  @Autowired
+  MessageChannel tcpServerOutboundChannel;
+
+  @Autowired
+  AbstractServerConnectionFactory connectionFactory;
+
+  @Autowired
+  BlockingQueue<String> tcpServerReceivedMessageQueue;
+
+  @Test
+  void connectAndSend() throws IOException, InterruptedException {
+    System.out.println("------------connectAndSend-------------");
+    // connect
+    try (Socket s = new Socket("localhost", 1234)) {
+      // send message to tcp server
+      s.getOutputStream().write("abc".getBytes());
+      s.getOutputStream().write("\r\n".getBytes());
+      s.getOutputStream().flush();
+
+      // get received message from queue
+      String message = tcpServerReceivedMessageQueue.poll(1, TimeUnit.SECONDS);
+      System.out.println(message);
+
+      // Work fine!!
+      Assertions.assertThat(message).isEqualTo("abc12");
+    }
+  }
+
+  @Test
+  void connectAndReceive() throws IOException {
+    System.out.println("------------connectAndReceive-------------");
+    // connect
+    try (Socket s = new Socket("localhost", 1234);
+        BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
+
+      // send message to remote client using server connection
+      connectionFactory.getOpenConnectionIds().stream().findAny().ifPresent(
+          connectionId -> tcpServerOutboundChannel.send(
+              MessageBuilder.withPayload("abc".getBytes()).setHeader(IpHeaders.CONNECTION_ID, connectionId).build()));
+
+      // receive message
+      String message = r.readLine();
+      System.out.println(message);
+
+      // I will expect "abc21" but it is become "abc1"...
+      Assertions.assertThat(message).isEqualTo("abc21");
+    }
+  }
+
+  static class LocalContext {
+    @Bean
+    @Order(1)
+    public TcpConnectionInterceptorFactory characterAppendingInterceptor1() {
+      return new SpiGh8609Application.CharacterAppendingInterceptor.Factory("1");
+    }
+
+    @Bean
+    @Order(2)
+    public TcpConnectionInterceptorFactory characterAppendingInterceptor2() {
+      return new SpiGh8609Application.CharacterAppendingInterceptor.Factory("2");
+    }
+
+  }
+}
