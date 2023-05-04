f306350290dc15df1263920029d3e78c10d2b596
Kazuki Shimizu <kazuki43zoo@gmail.com>
2023-05-05 07:07:50 +0900

    Polish javadoc

--- src/main/java/com/example/spigh8609/SpringIntegrationIssue8609PatchInterceptor.java
+++ src/main/java/com/example/spigh8609/SpringIntegrationIssue8609PatchInterceptor.java
@@ -3,11 +3,16 @@ package com.example.spigh8609;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.integration.ip.tcp.connection.TcpConnection;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptor;
 import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorFactoryChain;
 import org.springframework.integration.ip.tcp.connection.TcpConnectionInterceptorSupport;
 
 /**
  * The patch interceptor for fixing the <a href="https://github.com/spring-projects/spring-integration/issues/8609">GH-8609</a>.
+ * <p>
+ * This interceptor's factory should be applied to the {@link TcpConnectionInterceptorFactoryChain} at last.
+ * </p>
  */
 public class SpringIntegrationIssue8609PatchInterceptor extends TcpConnectionInterceptorSupport {
 
